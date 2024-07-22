84974cc2c2b8dc46705ac99b7c77830054e64448
ffreemt <yucongo+fmt@gmail.com>
2024-07-22 17:01:31 +0800

    first commit

---
+++ src/lmtr_agents/__init__.py
@@ -0,0 +1,5 @@
+"""Init."""
+__version__ = "0.1.0a0"
+from .lmtr_agents import lmtr_agents
+
+__all__ = ("lmtr_agents",)
---
+++ src/lmtr_agents/lmtr_agents.py
@@ -0,0 +1,7 @@
+"""Define lmtr_agents."""
+from loguru import logger
+
+
+def lmtr_agents():
+    """Define lmtr_agents."""
+    logger.debug(" entry ")
