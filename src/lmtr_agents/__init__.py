5aba01945c8226fdf50b7cd2cccbba25df9f5785
ffreemt <yucongo+fmt@gmail.com>
2024-08-10 15:35:15 +0800

    Bump to 0.1.0a2, fix all([...]) and MODEL_DUCK(..., api_key='NA')

--- src/lmtr_agents/__init__.py
+++ src/lmtr_agents/__init__.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from lmtr_agents.lmtr_agents import agent_tr, agent_ref, agent_imp, agent_comb, agent_comb, agent_comb_imp, agent_comb_imp1
 
 load_dotenv()
-__version__ = "0.1.0a1"
+__version__ = "0.1.0a2"
 
 # agent_model = os.getenv("AGENT_MODEL")
 # agent_base_url = os.getenv("AGENT_BASE_URL")
